7a2a6bee4e8d08f902f4c6106505be48e9707f3f
shvvkz <viggocasciano0301@gmail.com>
2025-06-20 10:42:56 +0200

    feat: creation of the port scanner

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -1,44 +1,66 @@
 pub mod ip;
-pub mod service;
 pub mod port;
+pub mod service;
+
+use std::net::IpAddr;
 
 use crate::config::ScanOptions;
-use crate::scanner::{ip::expand_ips, port::scan_port, service::get_service_name};
+use crate::scanner::{ip::expand_ips, port::scan_port};
 
-use tokio::task;
 use futures::stream::{FuturesUnordered, StreamExt};
+use tokio::task;
 
 pub async fn run_scan(options: ScanOptions) {
+    let t0 = std::time::Instant::now();
     let ips = expand_ips(&options.targets);
     let mut tasks = FuturesUnordered::new();
 
     for ip in ips {
         for &port in &options.ports {
-            let timeout = options.timeout;
-            let ip_copy = ip.clone();
             tasks.push(task::spawn(async move {
-                let is_open = scan_port(ip_copy, port, timeout).await;
-                (ip_copy, port, is_open)
+                let is_open = scan_port(ip, port, options.timeout, options.verbose).await;
+                (ip, port, is_open)
             }));
         }
     }
 
-    let mut results = Vec::new();
+    let mut results = ScanResultHandler::new();
 
     while let Some(Ok((ip, port, port_status))) = tasks.next().await {
-        results.push((ip, port, port_status));
+        results.add_result(ip, port, port_status);
+    }
+    println!("✅ Scan finished in {}s\n", t0.elapsed().as_secs_f32());
+    if options.output_format == crate::config::OutputFormat::Json {
+        let json_output = crate::output::json::output_json(results, options.open_only);
+        std::fs::write("portscanx.json", json_output).expect("Failed to write JSON output");
+    } else if options.output_format == crate::config::OutputFormat::Csv {
+        let csv_output = crate::output::csv::output_csv(results, options.open_only);
+        std::fs::write("portscanx.csv", csv_output).expect("Failed to write CSV output");
+    } else if options.output_format == crate::config::OutputFormat::Terminal{
+        crate::output::terminal::output_terminal(results, options.open_only);
+    }
+}
+
+pub struct ScanResultHandler {
+    pub open: Vec<(IpAddr, u16)>,
+    pub filtered: Vec<(IpAddr, u16)>,
+    pub closed: Vec<(IpAddr, u16)>,
+}
+
+impl ScanResultHandler {
+    pub fn new() -> Self {
+        ScanResultHandler {
+            open: Vec::new(),
+            filtered: Vec::new(),
+            closed: Vec::new(),
+        }
     }
 
-    println!("\n✅ Scan terminé. Résultats :\n");
-
-    for (ip, port, port_status) in &results {
-        if *port_status == port::PortStatus::Open {
-            let service = get_service_name(*port).unwrap_or("unknown");
-            println!("✔ {ip}:{port} → open ({service})");
-        } else if !options.open_only && *port_status == port::PortStatus::Filtered {
-            println!("✘ {ip}:{port} → filtered");
-        } else if !options.open_only && *port_status == port::PortStatus::Closed {
-            println!("✘ {ip}:{port} → closed");
+    pub fn add_result(&mut self, ip: IpAddr, port: u16, status: port::PortStatus) {
+        match status {
+            port::PortStatus::Open => self.open.push((ip, port)),
+            port::PortStatus::Filtered => self.filtered.push((ip, port)),
+            port::PortStatus::Closed => self.closed.push((ip, port)),
         }
     }
 }
