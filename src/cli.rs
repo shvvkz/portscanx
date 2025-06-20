845b490c9cf4960c62833da8c986649ae52f89fb
shvvkz <viggocasciano0301@gmail.com>
2025-06-20 11:01:24 +0200

    feat: add cli to portscanx

---
+++ src/cli.rs
@@ -0,0 +1,59 @@
+use clap::Parser;
+
+use crate::{config::{OutputFormat, ScanOptions}, scanner::service::parse_ports};
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "portscanx",
+    version,
+    disable_version_flag = true,
+    about = "A fast and flexible port scanner written in Rust.",
+)]
+pub struct Cli {
+    /// The IP address or hostname to scan.
+    #[arg(value_name = "IP/Hostname")]
+    pub target: String,
+
+    /// The port or range of ports to scan (e.g., 80, 1-1000).
+    #[arg(short, long, value_name = "PORT/RANGE", default_value = "1-65535")]
+    pub ports: String,
+
+    /// Timeout for each port scan in milliseconds (default: 1000).
+    #[arg(short, long, default_value_t = 1000)]
+    pub timeout: u64,
+
+    /// Output format: terminal, json, csv (default: terminal).
+    #[arg(short, long, default_value = "terminal")]
+    pub output: String,
+
+    /// Only show open ports.
+    #[arg(long)]
+    pub only_open: bool,
+
+    /// Enable verbose output.
+    #[arg(short, long)]
+    pub verbose: bool,
+}
+
+impl From<Cli> for ScanOptions {
+    fn from(cli: Cli) -> Self {
+        let targets = vec![cli.target];
+        let ports = parse_ports(&cli.ports);
+
+        let output_format = match cli.output.as_str() {
+            "json" => OutputFormat::Json,
+            "csv" => OutputFormat::Csv,
+            _ => OutputFormat::Terminal,
+        };
+
+        ScanOptions {
+            targets,
+            ports,
+            timeout: std::time::Duration::from_millis(cli.timeout),
+            verbose: cli.verbose,
+            output_format,
+            parallelism: 100, // ou tu peux ajouter une option plus tard
+            open_only: cli.only_open,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
+use portscanx::cli::Cli;
 use portscanx::scanner::run_scan;
 use portscanx::config::ScanOptions;
+use clap::Parser;
 
 #[tokio::main]
 async fn main() {
-    let options = ScanOptions {
-        targets: vec!["192.168.100.1".to_string()],
-        ports: vec![22, 80, 443, 8080, 3306, 5432, 6379, 65535, 1],
-        output_format: portscanx::config::OutputFormat::Terminal,
-        verbose: false,
-        timeout: std::time::Duration::from_millis(500),
-        parallelism: 100,
-        open_only: true,
-    };
-
+    let cli = Cli::parse();
+    let options = ScanOptions::from(cli);
     run_scan(options).await;
 }
